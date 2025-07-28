6a35183459c2e30bb45e75610ab7388793a45926
Leontev <leontev@yahoo.com>
2025-07-28 12:45:32 +0400

    feat: analytics api function name change

--- src/hooks/useAnalyticsData.ts
+++ src/hooks/useAnalyticsData.ts
@@ -4,9 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { type LocationAnalytics } from '../mocks/handlers';
 
 async function fetchAnalytics(): Promise<LocationAnalytics[]> {
-  const endpoint = import.meta.env.PROD
-    ? '/api/get-real-analytics'
-    : '/api/analytics';
+  const endpoint = import.meta.env.PROD ? '/api/analytics' : '/api/analytics';
 
   const res = await fetch(endpoint);
   if (!res.ok) throw new Error('Failed to fetch analytics');
